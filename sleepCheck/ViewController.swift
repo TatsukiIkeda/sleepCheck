92022854e46a9627fc31dc08647b35bc42c019a8
TatsukiIkeda <kisenan411@gmail.com>
2020-06-23 22:32:50 +0900

    Initial Commit

---
+++ sleepCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sleepCheck
+//
+//  Created by Tatsuki Ikeda on 2020/06/23.
+//  Copyright © 2020 tatsuki.ikeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
