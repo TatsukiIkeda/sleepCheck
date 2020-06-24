c293ebc0f71dece0f23f2453c9dca2acc38cbaa8
TatsukiIkeda <kisenan411@gmail.com>
2020-06-24 16:41:47 +0900

    Lottieアニメーション関数に変更簡略化 dateGet関数作成、現在時刻（日本時間）を取得

--- sleepCheck/Controller/ViewController.swift
+++ sleepCheck/Controller/ViewController.swift
@@ -12,42 +12,60 @@ import Lottie
 class ViewController: UIViewController {
 
     var animationView:AnimationView = AnimationView()
-
     let ainamtion = Animation.named("night")
     
-        
+    ///現在時間取得（ロンドン時間）
+    let dt = Date()
+    /// インスタンス生成
+    let dateFormatter = DateFormatter()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-        
-        
-        
-        
+       
+        dateGet()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    ///現在時間変換関数
+    func dateGet(){
+        ///ロンドン時間を日本時間に変換
+              dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "MMMdHm", options: 0, locale: Locale(identifier: "ja_JP"))
+        ///時間だけをdataに指定
+               let data = dateFormatter.string(from: dt).suffix(4)
+                print(data)
+    }
+    
+    
+    
+    ///Lottieアニメーション設定
+    func Lottienimation() {
+           animationView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: (view.frame.size.height) / 3)
+                  animationView.animation = ainamtion
+                  animationView.contentMode = .scaleAspectFit
+                  animationView.loopMode = .playOnce
+                  animationView.backgroundColor = .clear
+                  view.addSubview(animationView)
+       }
+    ///寝るボタンアニメーション
     @IBAction func nightLottieStart(_ sender: Any) {
-    
-    
-        animationView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
-        animationView.animation = ainamtion
-        animationView.contentMode = .scaleAspectFit
-        animationView.loopMode = .playOnce
-        animationView.backgroundColor = .clear
-        view.addSubview(animationView)
+        Lottienimation()
         animationView.play()
         
     }
-    
+    ///起きるボタンアニメーション
     @IBAction func nightLottieStop(_ sender: Any) {
+        Lottienimation()
         animationView.play()
         animationView.stop()
         
     }
     
-    
+   
     
 }
 
